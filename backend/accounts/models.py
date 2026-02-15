d7811e4da70f500c431dbf64da1577f09682e9aa
AliShahbazbaig <mirzaalishahbaz192@gmail.com>
2026-02-15 18:08:17 +0500

    Full project: frontend + backend

---
+++ backend/accounts/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager , PermissionsMixin
+
+# Custom user creation for email registration
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError("User must have an email address")
+        
+        email= self.normalize_email(email)
+        user= self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_supperuser",True)
+        return self.create_user(email,password,**extra_fields)
+
+
+# User Model
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    object = UserManager()
+
+    USERNAME_FIELD ='email'
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+     
+    def __str__(self)->str:
+        return self.email
+    
